7dc19b444736a1bc6782474d7e35a86398676714
Netanel Draiman <ndraiman@gmail.com>
2015-10-27 23:39:16 +0200

    started to implement  append method\n issue: segmentation fault

--- main.c
+++ main.c
@@ -1,6 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "slist.H"
 
+void print_list(slist_t*);
+
+
+
 int main() {
+    
+    slist_t* list = (slist_t*)malloc(sizeof(slist_t));
+    
+    slist_init(list);
+    int x = 5;
+    slist_append(list, &x);
+    print_list(list);
+    
+    return 0;
+}
 
 
+
+
+
+void print_list(slist_t* list) {
+    printf("print_list \n");
+    
+    slist_node_t* p;
+    
+    for(p = slist_head(list); p != NULL; p = slist_next(p)) {
+        
+        
+        printf("%s -> ", (char*)slist_data(p));
+    }
+    
+    printf("|| \n");
+    
 }
--- slist.c
+++ slist.c
@@ -5,9 +5,10 @@
 
 
 void slist_init(slist_t* list) {
+  printf("slist_init() \n");
   
   memset(list, 0, sizeof(list));
-  slist_head(list) = (slist_node_t*)malloc(sizeof(slist_node_t));
+  slist_head(list) = NULL;
   slist_tail(list) = slist_head(list);
   slist_size(list) = 0;
   
@@ -15,30 +16,27 @@ void slist_init(slist_t* list) {
 
 
 void slist_destroy(slist_t* list,slist_destroy_t freeData) {
+    printf("slist_destroy() \n");
     
     slist_node_t* p;
     int i;
     
-    if(freeData) {
-        
-        for(p = slist_head(list); p != NULL; p = slist_next(p)) {
-            free(slist_data(p));
-        }
-        
-    }
-    
-}
-
-
-void print(slist_t* list) {
-    
-    slist_node_t* p;
-    
     for(p = slist_head(list); p != NULL; p = slist_next(p)) {
-        
-        printf("%s -> ", (char*)slist_data(p));
+        if(freeData)
+            free(slist_data(p));
+            
+            
     }
     
-    printf("|| \n");
+}
+
+
+int slist_append(slist_t* list, void* newData) {
+    printf("slist_append() \n");
+    
+    if(slist_head(list) == NULL) {
+        slist_data(slist_head(list)) = newData;
+    }
     
 }
+
